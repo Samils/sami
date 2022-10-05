c2c64d29241d70826e7b5a57069186541c331ae3
agostinhosamil <agostinhosaml832@gmail.com>
2022-10-05 16:52:24 +0100

    feat(sami/cli): set clinter configurations up

--- cli/index.php
+++ cli/index.php
@@ -4,6 +4,14 @@ $commandLineInterface = requires ('command-line-interface');
 
 $cli = $commandLineInterface ();
 
+$cli->config (['src' => __DIR__])
+  ->setNamespace ('Application\Cli\Command')
+  ->registerCommandDir ('~/bin')
+  ->plugins ('module:samils-cli')
+  ->plugins ('module:samils-cli-*')
+  ->plugins ('module:samils-devpacks')
+  ->plugins ('module:xsami/cli')
+  ->plugins ('./plugins');
 $moduleConfig = requires ('~/module.json');
 
 if (is_array ($moduleConfig) &&
