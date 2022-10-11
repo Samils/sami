7b65a71cccfe43b9af873b24f29810647efe2ab2
agostinhosamil <agostinhosaml832@gmail.com>
2022-10-11 21:36:59 +0100

    fix(sami/cli/template): update template for controller

--- cli/templates/controller.template.php
+++ cli/templates/controller.template.php
@@ -1,18 +1,22 @@
-/**
- * <?= $props ['className'] ?>
- * -
- * Controller that'll manage
- * the application basic requests
- */
-class <?= $props ['className'] ?> extends SamiController {
-  <?php if (is_array ($props ['methods'])) {
-    foreach ($props ['methods'] as $method) { ?>
+namespace<?= $props ['namespace'] ? ' ' . $props ['namespace'] : '' ?> {
+  use SamiController;
 
   /**
-   * @method void <?= $method ?>
-
+   * <?= $props ['className'] ?>
+   * -
+   * Controller that'll manage
+   * the application basic requests
    */
-  function <?= $method ?> () {}
-  <?php }} ?>
+  class <?= $props ['className'] ?> extends SamiController {
+    <?php if (is_array ($props ['methods'])) {
+      foreach ($props ['methods'] as $method) { ?>
 
+    /**
+     * @method void <?= $method ?>
+
+     */
+    function <?= $method ?> () {}
+    <?php }} ?>
+
+  }
 }
