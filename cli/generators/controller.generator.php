1a47ca877f214978e215ee4133b0e027d4cbc88f
agostinhosamil <agostinhosaml832@gmail.com>
2022-10-11 21:38:20 +0100

    fix(sami/cli/generator): correct the generated controller class name and namespace path

--- cli/generators/controller.generator.php
+++ cli/generators/controller.generator.php
@@ -5,6 +5,8 @@ use Sammy\Packs\Sami\CommandLineInterface\Arguments;
 $module->exports = [
   'templates' => ['controller'],
 
+  'aliases' => ['c'],
+
   'controller.target' => 'app/controllers',
 
   'controller.rename' => function ($name) {
@@ -21,15 +23,29 @@ $module->exports = [
 
     $re = '/Controller$/i';
     $name = preg_replace ($re, '', $name);
+    $namespace = null;
 
-    $className = join ('', [ucfirst ($name), 'Controller']);
+    $className = join ('', [$name, 'Controller']);
+
+    $classNameSlices = preg_split ('/[\/\\\\]/', $className);
+
+    $classNameSlices = array_map (function ($slice) {
+      return snake2camelcase ($slice);
+    }, $classNameSlices);
+
+    $classNameSlicesLen = count ($classNameSlices);
+
+    if ($classNameSlicesLen >= 2) {
+      $namespace = join ('\\', array_slice ($classNameSlices, 0, -1 + $classNameSlicesLen));
+    }
 
     $parameters = Arguments::GetParameters ();
 
     $methods = $parameters->slice (2);
 
     return [
-      'className' => $className,
+      'namespace' => $namespace,
+      'className' => $classNameSlices [-1 + $classNameSlicesLen],
       'methods' => $methods
     ];
   }
